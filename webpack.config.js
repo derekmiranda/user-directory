86b12bd7cb721f01b6dab545b7e0712dffd4bf15
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-15 19:01:56 -0700

    Using default Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const WDS_PORT = 8000;
+
+module.exports = {
+  entry: [
+    './client',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/dist'),
+    publicPath: 'dist',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(jsx?)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: [ path.resolve(__dirname, './client') ],
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        include: [ path.resolve(__dirname, './client/css') ],
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  devServer: {
+    contentBase: './public',
+    inline: true,
+    hot: true,
+  },
+}
