65ce113a72a26ad529573c4819db7c86ff8f6e4d
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-17 23:13:40 -0700

    Laid out basic root reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,1 +1,25 @@
 import { combineReducers } from 'redux';
+import { REQUEST_USERS, RECEIVE_USERS } from './actions';
+
+const rootReducer = (state = {
+  isRequesting: false,
+}, action) => {
+  switch (action.type) {
+    case REQUEST_USERS:
+      return {
+        ...state,
+        isRequesting: true,
+      }
+    case RECEIVE_USERS:
+      const { userList } = action;
+      return {
+        ...state,
+        userList,
+        isRequesting: false,
+      }
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
