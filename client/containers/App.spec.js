c891bc45af5b72203ce5cd8535da10c6ef851ce0
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-17 22:15:43 -0700

    Created empty components/containers

--- client/containers/App.spec.js
+++ client/containers/App.spec.js
@@ -1,7 +1,12 @@
 import renderer from 'react-test-renderer';
+import NUM_USERS from '../constants';
 import App from './App';
 
-test('App renders correctly', () => {
+it('renders correctly', () => {
   const tree = renderer.create(App()).toJSON();
   expect(tree).toMatchSnapshot();
+})
+
+xit(`fetches ${NUM_USERS} users from randomuser.me`, (done) => {
+  
 })
--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import "babel-polyfill";
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './App';
+import App from './containers/App';
 
 render(
   <App/>,
