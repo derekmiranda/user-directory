114b4da0a27fede7e2979dee3f2ca72c0bc2d685
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-19 16:07:56 -0700

    Capitalizes necessary fields

--- client/containers/UserInfo.js
+++ client/containers/UserInfo.js
@@ -14,6 +14,6 @@ const mapStateToProps = (_, ownProps) => {
   }
 }
 
-const ConnectedUserInfo = connect()(UserInfoBox);
+const ConnectedUserInfo = connect(mapStateToProps)(UserInfoBox);
 
 export default ConnectedUserInfo;
