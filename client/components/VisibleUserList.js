09f4cbc4285a3eac72dbb49cb2b0a53d1287db5c
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-18 23:11:20 -0700

    Separeted user list into container and presentational components

--- client/components/VisibleUserList.js
+++ client/components/VisibleUserList.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const VisibleUserList = () => (
-  <div>
-    <h1>User Directory</h1>
-    {userListElem}
-  </div>
-)
+const VisibleUserList = (props) => {
+  const { userList } = props;
+
+  let userListElem;
+  if (userList) {
+    const userElems = userList.map(
+      ({ name }, idx) => <li key={idx}>{name.first} {name.last}</li>
+    )
+    userListElem = <ul>{userElems}</ul>;
+  }
+
+  return (
+    <div>
+      <h1>User Directory</h1>
+      {userListElem}
+    </div>
+  )
+}
 
 VisibleUserList.propTypes = {
   userList: PropTypes.array.isRequired,
--- client/containers/UserInfoList.js
+++ client/containers/UserInfoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
+import VisibleUserList from '../components/VisibleUserList';
 
 export class UserInfoList extends Component {
   componentDidMount() {
@@ -10,22 +11,7 @@ export class UserInfoList extends Component {
   }
 
   render() {
-    const { userList } = this.props;
-
-    let userListElem;
-    if (userList) {
-      const userElems = userList.map(
-        ({ name }, idx) => <li key={idx}>{name.first} {name.last}</li>
-      )
-      userListElem = <ul>{userElems}</ul>;
-    }
-
-    return (
-      <div>
-        <h1>User Directory</h1>
-        {userListElem}
-      </div>
-    )
+    return VisibleUserList(this.props);
   }
 }
 
