9d1ad705b326d207e5585f3104593ef7c4a606ff
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-20 13:17:52 -0700

    Added responsive grid system

--- client/components/UserInfoSection.js
+++ client/components/UserInfoSection.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import UserInfo from '../containers/UserInfo';
+import { Container, Col } from 'react-grid-system'
 
 const UserInfoSection = ({ letter, users }) => {
+  const userInfoBoxes = users && users.map((user, i) => (
+    <Col xs={6} md={4} lg={3} key={i}>
+      <UserInfo {...user}/>
+    </Col>
+  ))
+
   return (
-    <section>
+    <Container style={{ margin: 0 }}>
       <h2>{letter}</h2>
       <hr></hr>
-      {
-        users && users.map((user, i) => {
-          return <UserInfo {...user} key={i} />;
-        })
-      }
-    </section>
+      {userInfoBoxes}
+    </Container>
   )
 }
 
