8c42279b6f272609048f94e5fc1c859e73c93961
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-19 23:28:19 -0700

    Uglify client on production

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,51 @@ const webpack = require('webpack');
 
 const WDS_PORT = 8000;
 
-module.exports = {
-  entry: [
-    './client',
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/dist'),
-    publicPath: 'dist',
-  },
-  plugins: [
+const productionMode = process.env.NODE_ENV === "production";
+const plugins = productionMode ?
+  [
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compress: {
+        warnings: false,
+        drop_console: true
+      },
+    })
+  ] :
+  [
     new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.(jsx?)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        include: [ path.resolve(__dirname, './client') ],
-      },
-      {
-        test: /(\.css|\.scss|\.sass)$/,
-        include: [ path.resolve(__dirname, './client/css') ],
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ]
-  },
-  devServer: {
-    contentBase: './public',
-    inline: true,
-    hot: true,
-  },
-}
+  ]
+
+const devPlugins =
+
+  module.exports = {
+    entry: [
+      './client',
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public/dist'),
+      publicPath: 'dist',
+    },
+    plugins,
+    module: {
+      loaders: [
+        {
+          test: /\.(jsx?)$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          include: [path.resolve(__dirname, './client')],
+        },
+        {
+          test: /(\.css|\.scss|\.sass)$/,
+          include: [path.resolve(__dirname, './client/css')],
+          loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ]
+    },
+    devServer: {
+      contentBase: './public',
+      inline: true,
+      hot: true,
+    },
+  }
