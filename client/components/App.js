4b1fa827a464424dedc9919c658c12c3067ac113
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-20 12:36:06 -0700

    Added font

--- client/components/App.js
+++ client/components/App.js
@@ -3,8 +3,12 @@ import UserInfoList from '../containers/UserInfoList';
 import Sorter from './Sorter';
 import SearchBar from './SearchBar';
 
+const style = {
+  fontFamily: 'Calibri, sans-serif',
+}
+
 const App = () => (
-  <div>
+  <div id="app" style={style}>
     <h1>User Directory</h1>
     <SearchBar />
     <Sorter />
